607e57442a942e9c60892ace989626a75f888d1d
Izhal_Brown <30791244+Izhal-Brown@users.noreply.github.com>
2022-09-08 12:01:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -32,9 +32,4 @@ app.use((req, res, next) => { //membuat halaman jika halaman yang dituju ngga ad
   })
 })
 
-
-let port = process.env.PORT || 8080;
-
-app.listen(port, () => {
-  console.log('App is running at the port ${port}')
-})
+app.listen(3000, () => console.log('server: http://localhost:3000'))
